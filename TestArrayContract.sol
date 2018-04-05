88e7ea49aea60cbc70e940efef8d24d45040ddee
joe <joe@joes-MacBook-Pro.local>
2018-04-05 15:19:11 -0400

    clean

--- TestArrayContract.sol
+++ TestArrayContract.sol
@@ -2,15 +2,8 @@ pragma solidity ^0.4.15;
 
 import {ArrayTest} from './ArrayTest.sol';
 
-import {ArrayTest as ArrayContract} from './ArraysContract.sol';
-
-
 /**
-
-
     Test Deployment
-
-
 
 
  */
@@ -20,17 +13,12 @@ contract TestArrayContract {
 
   ArrayTest public arrayTest;
 
-  ArrayContract public arrayContract;
-
   address[] public addrs_ = [0x1234, 0x12345];
-  uint256[] public uints_;
 
   function TestArrayContract() public {
 
 
     arrayTest = new ArrayTest(addrs_);
-
-    arrayContract = new ArrayContract(addrs_);
 
     assert(arrayTest.getAddress(0) == addrs_[0] && addrs_[0] == 0x1234);
 
@@ -45,19 +33,12 @@ contract TestArrayContract {
 
     arrayTest.setUints(uints);
 
-
-    uint256[] memory uints__ = new uint256[](10);
     for (uint256 j = 0; j < 10; j++) {
 
       uint256 cur = arrayTest.getUints(j);
 
-      uints__[j] = cur;
-
-      assert(cur == uints__[j]);
+      assert(cur == uints[j] && cur == uint256(j * 100));
     }
-
-    uints_ = uints__;
-
 
   }
 }
